5e7453633b99fa07a1781626813657fe8af9b6b9
Josh Wang <josh@jlwang.org>
2024-03-30 22:56:59 -0400

    completed portable keymaps v1

--- users/jlw/combos.h
+++ users/jlw/combos.h
@@ -1,7 +1,9 @@
 const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W,    COMBO_END};
 const uint16_t PROGMEM combo_del[] = {KC_Y, KC_QUOT, COMBO_END};
+const uint16_t PROGMEM combo_boot[] = {KC_Q, KC_Z, KC_QUOT, KC_SLSH, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_esc, KC_ESC),
     COMBO(combo_del, KC_DEL),
+    COMBO(combo_boot, QK_BOOT)
 };
